2b8365f91ce8f6c5d5ec0518074483ded03c9665
zerutis <rokas.zeruolis@gmail.com>
2022-11-26 23:02:08 +0200

    added getters and setters to model

--- src/main/java/com/zerutis/codingchallenge/model/BankAccountStatement.java
+++ src/main/java/com/zerutis/codingchallenge/model/BankAccountStatement.java
@@ -28,4 +28,64 @@ public class BankAccountStatement {
 
     @Column(name = "currency")
     private String currency;
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public LocalDateTime getOperationDateTime() {
+        return operationDateTime;
+    }
+
+    public void setOperationDateTime(LocalDateTime operationDateTime) {
+        this.operationDateTime = operationDateTime;
+    }
+
+    public String getBeneficiary() {
+        return beneficiary;
+    }
+
+    public void setBeneficiary(String beneficiary) {
+        this.beneficiary = beneficiary;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    @Override
+    public String toString() {
+        return "BankAccountStatement{" +
+                "accountNumber='" + accountNumber + '\'' +
+                ", operationDateTime=" + operationDateTime +
+                ", beneficiary='" + beneficiary + '\'' +
+                ", comment='" + comment + '\'' +
+                ", amount=" + amount +
+                ", currency='" + currency + '\'' +
+                '}';
+    }
 }
